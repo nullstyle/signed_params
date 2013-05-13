b2f4ae8c8e9790c2395d2c556f0c3e3aec29ad56
Scott Fleckenstein <nullstyle@gmail.com>
2013-05-12 17:10:29 -0700

    Adds controller method helpers

--- lib/signed_params/controller_methods.rb
+++ lib/signed_params/controller_methods.rb
@@ -1,5 +1,45 @@
+require 'active_support/concern'
+
 module SignedParams
   module ControllerMethods
+    extend ActiveSupport::Concern
+
+    included do |base|
+      base.send(:before_filter, :setup_for_signed_params)
+    end
+
+    private
+    def verify_params!(signer)
+      verification_results =  signer.verify_params(params)
+
+      verification_results.each do |(key, value, viewer, status)|
+        next if status == :not_signed
+
+        if status == :success
+          params[key] = value
+          @signed_params_viewers[key] = viewer
+        else
+          @signing_errors[key] = status
+        end
+      end
+    end
+
+    def params_verified?
+      @signing_errors.any?
+    end
+
+    def setup_for_signed_params
+      @signed_params_viewers = {}
+      @signing_errors = {}
+    end
+
+    def signing_errors
+      @signing_errors
+    end
+
+    def signed_params_viewers
+      @signed_params_viewers
+    end
 
   end
 end
--- lib/signed_params/railtie.rb
+++ lib/signed_params/railtie.rb
@@ -1,5 +1,7 @@
 module SignedParams
   class Railtie < Rails::Railtie
-    
+    initializer "signed_params.install_controller_methods" do |app|
+      ActionController::Base.send(:include, SignedParams::ControllerMethods)
+    end
   end
 end
